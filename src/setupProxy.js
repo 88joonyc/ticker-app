d8fd55900b884a5fe2f2f6566d430cb3cb39cd6e
88joonyc <joonyc88@gmail.com>
2023-05-10 14:26:27 -0400

    remove-proxy

---
+++ src/setupProxy.js
@@ -0,0 +1,7 @@
+module.exports = function(app) {
+    app.use(
+        createProxyMiddleware(["/api"], {
+            target: "https://polygon-ticker-app-production.up.railway.app/",
+        }),
+    );
+};
