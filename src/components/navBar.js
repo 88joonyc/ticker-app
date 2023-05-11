7b00f9c2d9e42a96bc9c7f4c719311448c76f5b2
88joonyc <joonyc88@gmail.com>
2023-05-10 23:11:40 -0400

    navv

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -39,9 +39,7 @@ export default function NavBar() {
         const delayDebounceSearch = setTimeout(async () => {
             if (searchQuery) {
                 try {
-                    const response = await fetch(`https://polygon-ticker-app-production.up.railway.app/api/ticker/search/by/${searchQuery}`, {
-                        headers: {'Access-Control-Allow-Origin': "*"}
-                    })
+                    const response = await fetch(`http://ticker-app-api-production.up.railway.app/api/ticker/search/by/${searchQuery}`)
                     let data 
                     if (response.ok) {
                         data = await response.json()
