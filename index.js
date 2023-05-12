ac7bae7a4093e42beb8121185c70ab8a3e3594bd
88joonyc <joonyc88@gmail.com>
2023-05-12 12:57:51 -0400

    build-path

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "./build")));
 app.get("*", function (_, res) {
   res.sendFile(
-    path.join(__dirname, "./client/build/index.html"),
+    path.join(__dirname, "./build/index.html"),
     function (err) {
       res.status(500).send(err);
     }
