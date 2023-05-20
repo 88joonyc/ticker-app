03af506f7787834ee0f92483d5a22baebb592a58
88joonyc <joonyc88@gmail.com>
2023-05-20 04:08:21 -0400

    revert-stock

--- src/store/stock.js
+++ src/store/stock.js
@@ -1,30 +1,14 @@
 import { csrfFetch } from "./csrf";
 
 const LOAD = 'stock/load'
-const PURCHASE_STOCK = 'stock/purchaseStock';
-const UPDATE_STOCK = 'stock/updateStock';
 
 const load = stocks => ({
     type: LOAD,
     payload: stocks
 });
 
-const purchaseStock = (stock) => {
-    return {
-        type: PURCHASE_STOCK,
-        payload: stock,
-    };
-};
-
-const updateStock = (stock) => {
-    return {
-        type: UPDATE_STOCK,
-        payload: stock
-    }
-};
-
 export const stocks = id => async dispatch => {
-    const response = await csrfFetch(`/api/stock/${id}`)
+    const response = await csrfFetch(`${process.env.REACT_APP_RAILWAY_BACK_URL}/api/stock/${id}`)
     const data = await response.json();
 
     dispatch(load(data.stocks))
@@ -32,59 +16,20 @@ export const stocks = id => async dispatch => {
     return response
 }
 
-export const purchase = (stock) => async dispatch => {
-
-    const response = await csrfFetch(`/api/stock`, {
-        method: 'POST',
-        body: JSON.stringify(stock)
-    })
-    
-    const data = await response.json();
-
-    dispatch(purchaseStock(data))
-    return data
-
-}
-
 const initialState = { stock: [] };
-
 
 const stockReducer = (state = initialState, action) => {
     let newState;
     switch(action.type) {
         case LOAD:
-            const stockList = {}
-            action.payload.forEach(stock => {
-                stockList[stock.id] = stock
-            })
             newState = Object.assign({}, state);
             newState.stock = [...action.payload];
-            newState.mark = stockList
-
             return newState
-        case PURCHASE_STOCK:
-            // if this is a new purchase, add the new item to the end 
-            if (action.payload.response.response.type === "purchase") {
-                newState = Object.assign({}, ...state.stock);
-                newState.stock = [...state.stock, action.payload.response]
-                return newState
-            } else {
-                // othewise we need to update the proper item 
-                state.mark[action.payload.response.response.id] = action.payload.response.response
-                let temp = []
-                for (const [key, val] of Object.entries(state.mark)){
-                    temp.push(val)
-                }
-                newState = Object.assign({});
-
-                newState.stock = [...temp];
-                newState.mark = state.mark;
-                return newState
-            }
         default:
             return state
     }
 
 } 
+
 
 export default  stockReducer
