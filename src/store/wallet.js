173bba51cff8a61f839def66f769d23621dc7a55
88joonyc <joonyc88@gmail.com>
2023-05-23 17:05:28 -0400

    response

--- src/store/wallet.js
+++ src/store/wallet.js
@@ -67,7 +67,7 @@ export const update = (wallet) => async (dispatch) => {
     const data = await response.json();
     dispatch(updateWallet(data.wallet));
     
-    return data
+    return response
 
 };
 
