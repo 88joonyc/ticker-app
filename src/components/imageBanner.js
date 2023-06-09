e063bc65ed606b19593e2539bcacc159a52d733a
88joonyc <joonyc88@gmail.com>
2023-06-09 01:38:54 -0400

    splashpage-heights

--- src/components/imageBanner.js
+++ src/components/imageBanner.js
@@ -11,11 +11,11 @@ export const ImageBanner = function ({data}) {
             <div className={`bg-${outline?.color} w-full text-${outline?.text} text-center md:text-left`}>
                 <div className="grid md:grid-cols-[1fr,1fr]">
                     <div className=' md:block'>
-                        <img className='h-full object-cover' src={`${info.image}`}/>
+                        <img className='h-[300px] md:h-full object-cover' src={`${info.image}`}/>
                     </div>
-                    <div className="flex flex-col md:h-[45rem] md:w-[360px] ml-4 md:ml-8">
+                    <div className="flex flex-col md:h-[45rem] md:w-[360px] md:ml-8">
                         <div className="my-auto">
-                            <h2 className="text-6xl mt-8 md:mt-0 font-light mb-10">{info.title}</h2>
+                            <h2 className="text-4xl md:text-6xl mt-8 md:mt-0 font-light mb-10">{info.title}</h2>
                             <div className="text-xl pb-8">{info.subtitle}</div>
                             <div className="text-lg font-light pb-10">{info.aux}</div>
                             <button className="px-8 py-4 border rounded-full">{info.button}</button>
@@ -23,7 +23,7 @@ export const ImageBanner = function ({data}) {
                     </div>
                 </div>
                 <div className="h-20 flex items-center justify-center text-sm">
-                    <div className='mx-4'>
+                    <div className='text-[8px] leading-tight md:text-base mx-4'>
                         Stocks & funds offered through Robinhood Financial. Other fees may apply. See our Fee Schedule for more details.
                     </div>
                 </div>
--- src/components/imageButton.js
+++ src/components/imageButton.js
@@ -3,11 +3,11 @@ export const ImageButton = function ({image}) {
     
     return (
         <>
-            <div className="relative h-full">
-                <img className="absolute h-[1000px] object-cover w-full" src={image} />
-                <div className="relative h-[1000px] md:p-20 flex flex-col text-5xl md:text-7xl font-light text-white text-center mx-auto md:w-[900px]">
+           <div className="relative h-full">
+                <img className="absolute h-[500px] md:h-[800px] object-cover w-full" src={image} />
+                <div className="relative h-[500px] md:h-[800px] md:p-20 flex flex-col text-3xl md:text-7xl font-light text-white text-center mx-auto md:w-[900px]">
                     <div className="mt-20 md:mt-0">Join a new generation of vigilantes</div>
-                    <a href="/signup" className="text-lg px-8 py-2 mx-auto border rounded-full mt-10 bg-black font-bold hover:bg-gray-900">Sign up</a>
+                    <a href="/signup" className="text-sm md:text-lg px-8 py-2 mx-auto border rounded-full mt-10 bg-black font-bold hover:bg-gray-900">Sign up</a>
                 </div>
             </div>
         </>
