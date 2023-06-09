c7a636dea338a3b122d91c0e04705e47544d80d3
88joonyc <joonyc88@gmail.com>
2023-06-09 15:03:49 -0400

    sidepanel

--- src/components/sidePanel.js
+++ src/components/sidePanel.js
@@ -9,32 +9,30 @@ export default function SidePanel ({data, list}) {
 
     return (
         <>
-            <div className='border md:h-[90vh] md:mb-0 overflow-y-scroll no-scrollbar text-xs' >
+            <div className='border md:h-[90vh] md:mb-0 overflow-y-scroll no-scrollbar text-xs md:sticky top-[60px]' >
                 <div className='w-full border-b px-2 py-4 '>
                     <div className='w-full '>Stocks</div>
                 </div>
                 <div>
                     {stocks&&data&&stocks?.map(stock => (
                         <div key={`stock-identifier-${stock.id}`}>
-                            <Link to={`/ticker/${stock.ticker}`}>
+                            <Link to={`/ticker/${stock?.ticker}`} >
                                 <div className={`px-2 py-1 flex justify-between hover:bg-gray-100 text-black`}>
                                     <div className='flex flex-col justify-center'>
-                                        <span className='font-bold'>{stock.ticker}</span>
-                                        <span className='font-light'>{stock.qty} {stock.qty > 1 ? 'shares' : 'share'}</span>
+                                        <span className='font-bold'>{stock?.ticker}</span>
+                                        <span className='font-light'>{stock?.qty} {stock?.qty > 1 ? 'shares' : 'share'}</span>
                                     </div>
                                     <div className='h-16'>
-                        
-                                    <VictoryChart >
-                                        <VictoryGroup     >
-                                            <VictoryLine data={data?.[stock?.ticker]}  style={{  data: { stroke:data?.[stock?.ticker]?.[0]?.close > stock.originalPrice  ? "#22c55e" : "#ef4444", strokeWidth: 2} }} y="close"  />
-                                            <VictoryAxis  style={{ ticks: {stroke: "grey"} }} invertAxis offsetY={150} tickFormat={() => ''} />
-                                        </VictoryGroup>
-                                    </VictoryChart>
-
+                                        <VictoryChart >
+                                            <VictoryGroup     >
+                                                <VictoryLine data={data?.[stock?.ticker]}  style={{  data: { stroke:data?.[stock?.ticker]?.[0]?.close > stock?.originalPrice  ? "#22c55e" : "#ef4444", strokeWidth: 2} }} y="close"  />
+                                                <VictoryAxis  style={{ ticks: {stroke: "grey"} }} invertAxis offsetY={150} tickFormat={() => ''} />
+                                            </VictoryGroup>
+                                        </VictoryChart>
                                     </div>
                                     <div className='flex items-center'>
                                         <span>{}</span>
-                                        <span>{((data?.[stock?.ticker]?.[0]?.close - stock.originalPrice)*stock.qty)?.toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</span>
+                                        <span>{((data?.[stock?.ticker]?.[0]?.close - stock?.originalPrice)*stock?.qty).toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</span>
                                     </div>
                                 </div>
                             </Link>
