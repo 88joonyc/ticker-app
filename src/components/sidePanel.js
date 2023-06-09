b3966e5b3e67c0c2826c77c93e6022611a0f2cff
88joonyc <joonyc88@gmail.com>
2023-06-09 15:54:54 -0400

    sidepanel-edit

--- src/components/sidePanel.js
+++ src/components/sidePanel.js
@@ -23,16 +23,18 @@ export default function SidePanel ({data, list}) {
                                         <span className='font-light'>{stock?.qty} {stock?.qty > 1 ? 'shares' : 'share'}</span>
                                     </div>
                                     <div className='h-16'>
-                                        <VictoryChart >
-                                            <VictoryGroup     >
-                                                <VictoryLine data={data?.[stock?.ticker]}  style={{  data: { stroke:data?.[stock?.ticker]?.[0]?.close > stock?.originalPrice  ? "#22c55e" : "#ef4444", strokeWidth: 2} }} y="close"  />
-                                                <VictoryAxis  style={{ ticks: {stroke: "grey"} }} invertAxis offsetY={150} tickFormat={() => ''} />
-                                            </VictoryGroup>
-                                        </VictoryChart>
+                        
+                                    <VictoryChart >
+                                        <VictoryGroup >
+                                            <VictoryLine data={data?.[stock?.ticker]}  style={{  data: { stroke:data?.[stock?.ticker]?.[0]?.close > stock?.originalPrice  ? "#22c55e" : "#ef4444", strokeWidth: 2} }} y="close"  />
+                                            <VictoryAxis  style={{ ticks: {stroke: "grey"} }} invertAxis offsetY={150} tickFormat={() => ''} />
+                                        </VictoryGroup>
+                                    </VictoryChart>
+
                                     </div>
-                                    <div className='flex items-center'>
-                                        <span>{}</span>
-                                        <span>{((data?.[stock?.ticker]?.[0]?.close - stock?.originalPrice)*stock?.qty).toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</span>
+                                    <div className='flex flex-col justify-center gap-2 items-center'>
+                                        <div>{data?.[stock?.ticker]?.[0]?.close.toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</div>
+                                        <div className={data?.[stock?.ticker]?.[0]?.close - stock?.originalPrice > 0 ? 'text-green-500' : 'text-red-500'}>{(data?.[stock?.ticker]?.[0]?.close - stock?.originalPrice)>0&&'+'}{(((data?.[stock?.ticker]?.[0]?.close - stock?.originalPrice)*stock?.qty)/(stock?.originalPrice)*stock?.qty).toFixed(2)}%</div>
                                     </div>
                                 </div>
                             </Link>
