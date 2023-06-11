71ef3202ad7117579b9514604dffc83052f68e63
88joonyc <joonyc88@gmail.com>
2023-06-11 11:54:06 -0400

    navbar-mobile

---
+++ src/components/navBar-mobile.js
@@ -0,0 +1,14 @@
+import react from 'react';
+import { FaCircle } from "react-icons/fa";
+
+
+export default function NavBarMobile () {
+    return (
+        <>
+            <div className='sticky bottom-0'>
+
+                <FaCircle />
+            </div>
+        </>
+    )
+}
