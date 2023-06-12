aee515aa0762f40720c56c68ad859697e74f2f79
88joonyc <joonyc88@gmail.com>
2023-06-12 09:59:36 -0400

    navbar-movbile

--- src/components/navBar-mobile.js
+++ src/components/navBar-mobile.js
@@ -1,13 +1,15 @@
 import react from 'react';
 import { FaCircle } from "react-icons/fa";
 
-
 export default function NavBarMobile () {
     return (
         <>
-            <div className='sticky bottom-0'>
-
-                <FaCircle />
+            <div className=' md:hidden sticky bottom-0 py-10 bg-white'>
+                <div className='flex justify-around'>
+                    <FaPowerOff />
+                    <FaLaptop />
+                    <FaAddressCard />
+                </div>
             </div>
         </>
     )
