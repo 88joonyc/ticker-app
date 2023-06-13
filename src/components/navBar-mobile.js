a655f017be1cae974c9b496c4d64686de9ad4cf2
88joonyc <joonyc88@gmail.com>
2023-06-13 00:33:28 -0400

    navbarmobile

--- src/components/navBar-mobile.js
+++ src/components/navBar-mobile.js
@@ -1,14 +1,23 @@
 import react from 'react';
-import { FaCircle } from "react-icons/fa";
+import { FaCircle, FaPowerOff, FaAddressCard, FaLaptop, FaMoneyBill } from "react-icons/fa";
 
 export default function NavBarMobile () {
+    const href = window.location.href
+    
     return (
         <>
-            <div className=' md:hidden sticky bottom-0 py-10 bg-white'>
+            <div className=' md:hidden sticky bottom-0 py-6 bg-white z-[100]'>
                 <div className='flex justify-around'>
-                    <FaPowerOff style={{ fontSize: '25px' }} />
-                    <FaLaptop style={{ fontSize: '25px' }} />
-                    <FaAddressCard style={{ fontSize: '25px' }} />
+                    <Link to='/'>
+                        <FaLaptop style={{ fontSize: '25px', color: `${ ( href === 'https://ticker-app-production.up.railway.app/') ? '#000000' : '#9f93a6'}` }} />
+                    </Link>
+                    <FaAddressCard style={{ fontSize: '25px', color: '#9f93a6' }} />
+                    {/* <button onClick={() => setOpenWallet(true)}> */}
+                        <FaMoneyBill style={{ fontSize: '25px', color: `${!openWallet ? '#9f93a6' : '#000000'}` }} />
+                    {/* </button> */}
+                    {/* <button onClick={() => setShowMenu(true)}> */}
+                        <FaPowerOff style={{ fontSize: '25px', color: `${!showMenu ? '#9f93a6' : '#000000'}` }} />
+                    {/* </button> */}
                 </div>
             </div>
         </>
