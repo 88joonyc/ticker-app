659aacedfaa7b5b61be929853123a8ac70388cee
88joonyc <joonyc88@gmail.com>
2023-06-13 12:41:58 -0400

    home-ml5

--- src/components/wallet.js
+++ src/components/wallet.js
@@ -56,7 +56,7 @@ export default function Wallet ({openWallet, setOpenWallet}) {
             <div className={`transition-[height]  linear duration-[.2s] ${toggle ? "h-[265px] md:h-[305px]" : " h-[130px]"} relative`}>
                 <div className="relative ">
                     <div onClick={() =>  setToggle(toggle)} className={`w-full h-20 py-8 border-t border-b ${toggle ? ' md:rounded-t border' : 'text-black bg-white'} hover:cursor-pointer flex flex-col justify-center border-b`}>
-                        <div className='flex mx-2 md:mx-6 flex-col justify-between relative '>
+                        <div className='flex mx-5 md:mx-6 flex-col justify-between relative '>
                             <div className="flex justify-between text-lg md:text-lg">
 
                                 <span className='font-bold'>{ wallet ? 'Buying Power' : 'Add Wallet'}</span>
