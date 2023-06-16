91ff5f613c69f6b98a88d6a601b348fdc11a7ec2
88joonyc <joonyc88@gmail.com>
2023-06-16 12:32:30 -0400

    mainRoutes

--- src/components/mainRoutes.js
+++ src/components/mainRoutes.js
@@ -9,7 +9,7 @@ import Home from "../pages/Home";
 import NavBarMobile from "./navBar-mobile";
 
 
-export default function MainRoutes ({isLoaded}) {
+export default function MainRoutes ({isLoaded, stocksData, total, current, list}) {
 
     const session = useSelector(state => state.session.user);
 
@@ -17,7 +17,7 @@ export default function MainRoutes ({isLoaded}) {
         <>      
         <NavBar />
         <Routes>
-            <Route  path='/' element={<> <Home isLoaded={isLoaded} /></>} />
+            <Route  path='/' element={<> <Home isLoaded={isLoaded} stocksData={stocksData} total={total} current={current} list={list} /></>} />
             {/* <Route  path='/ticker/:ticker' element={<Ticker /> } /> */}
         </Routes>
         <NavBarMobile/>
