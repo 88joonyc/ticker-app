ddad7593fad4fde78ac85e55b5d1d9854953d3b2
88joonyc <joonyc88@gmail.com>
2023-06-16 16:35:00 -0400

    searrchbar

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -14,7 +14,7 @@ export default function SearchBar ({full}) {
         const delayDebounceSearch = setTimeout(async () => {
             if (searchQuery) {
                 try {
-                    const response = await fetch(`/api/ticker/search/by/${searchQuery}`)
+                    const response = await fetch(`${process.env.REACT_APP_RAILWAY_BACK_URL}/api/ticker/search/by/${searchQuery}`)
                     let data 
                     if (response.ok) {
                         data = await response.json()
