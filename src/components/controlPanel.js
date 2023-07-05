8db9613572ac4e21aa6a3775cd0ef01567dbdd3a
88joonyc <joonyc88@gmail.com>
2023-07-05 13:26:02 -0400

    control-chooser

--- src/components/controlPanel.js
+++ src/components/controlPanel.js
@@ -81,7 +81,10 @@ export default function ControlPanel ({ticker, data}) {
         <>
             <div className='md:max-w-[400px] relative'>
                 <div className='flex flex-col border border-gray-200 pt-6  sticky top-[120px] shadow-lg'>
-                    <div className='text-xl mb-8 font-bold capitalize text-midnightPurple px-6'>{control + ' ' + ticker}</div>
+                    <div className='flex'>
+                        <button onClick={() => setControl('buy')} className={`text-xl mb-8 font-bold capitalize hover:text-highlightPurple px-6 ${control == 'buy' ? 'text-highlightPurple border-b-4 border-highlightPurple' : 'text-midnightPurple' }`}>Buy{' ' + ticker}</button>
+                        <button onClick={() => setControl('sell')} className={`text-xl mb-8 font-bold capitalize hover:text-highlightPurple px-6 ${control != 'buy' ? 'text-highlightPurple border-b-4 border-highlightPurple' : 'text-midnightPurple' }`}>Sell{ ' ' + ticker} </button>
+                    </div>
                     <form onSubmit={submitPurchase} className='flex flex-col px-6'>
                         <label className='text-base flex justify-between'> Buy in
                             <select className='border bg-white p-2 w-[150px] ' onChange={e => setType(e.target.value)}>
